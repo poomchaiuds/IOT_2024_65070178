92d28439a959569c1c48cafca3c5a13d0aab7ebb
poomchaiuds <109953357+poomchaiuds@users.noreply.github.com>
2024-07-29 23:00:21 +0700

    Update models.py

--- models.py
+++ models.py
@@ -11,6 +11,9 @@ class Book(Base):
     author = Column(String, index=True)
     year = Column(Integer, index=True)
     is_published = Column(Boolean, index=True)
+    description = Column(String, index=True)
+    sub = Column(String, index=True)
+    category = Column(String, index=True)
 
 class Coffee(Base):
     __tablename__ = 'coffees'
